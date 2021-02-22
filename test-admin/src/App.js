3e280c92b43efb47048180f39c2a34ba9f1c8dc7
Takashi Ikki <takahashi@omicale.com>
2021-02-22 16:23:21 +0900

    edit form

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import {Admin, Resource} from 'react-admin';
 import {UserList} from './users';
+import {PostList, PostEdit} from './posts';
 import jsonServerProvider from 'ra-data-json-server';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider}>
+        <Resource name="posts" list={PostList} edit={PostEdit}/>
         <Resource name="users" list={UserList}/>
     </Admin>
 );
