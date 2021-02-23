a3b813c5727b313b20d87e68ee2c125ccde33908
Takashi Ikki <takahashi@omicale.com>
2021-02-23 22:58:06 +0900

    add dashboard

--- test-admin/src/App.js
+++ test-admin/src/App.js
@@ -2,13 +2,15 @@ import * as React from "react";
 import {Admin, Resource} from 'react-admin';
 import {UserList} from './users';
 import {PostList, PostEdit, PostCreate} from './posts';
+import Dashboard from "./Dashboard";
 import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
+
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dataProvider={dataProvider}>
+    <Admin dashboard={Dashboard} dataProvider={dataProvider}>
         <Resource name="posts" list={PostList} create={PostCreate} edit={PostEdit} icon={PostIcon}/>
         <Resource name="users" list={UserList} icon={UserIcon}/>
     </Admin>
